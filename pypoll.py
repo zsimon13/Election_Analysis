3e2259a0a4e07ba30071349b0b9ae2a678d2841e
zsimon13 <102814578+zsimon13@users.noreply.github.com>
2022-05-04 21:46:48 -0500

    adding pypoll.py file

---
+++ pypoll.py
@@ -0,0 +1,6 @@
+# the data we need to retrieve
+# 1. The total number of votes cast
+# 2. The complete list of candiadates who recieved votes
+# 3. The percentage of votes each candidate won
+# 4. The total number of votes each candidate won
+# 5. The winner of the election based on popular vote.
